4ef5b0c0454597cee9f8f50df57cd8f331d47a0d
jgruenbaum <jeff.gruenbaum@gmail.com>
2021-04-11 17:01:06 -0500

    updated test for encryptionfile

--- tests/test.py
+++ tests/test.py
@@ -5,8 +5,9 @@ from pytest import fixture, mark
 sys.path.append(path.abspath(path.join(path.dirname(__file__),
                 path.pardir)))
 
-from fileflamingo.BaseFile import BaseFile  # noqa: E402
-from fileflamingo.RSAFile import RSAFile  # noqa: E402
+from fileflamingo.BaseFile import BaseFile              # noqa: E402
+from fileflamingo.RSAFile import RSAFile                # noqa: E402
+from fileflamingo.EncryptionFile import EncryptionFile  # noqa: E402
 
 
 @fixture
@@ -30,7 +31,16 @@ def env_setup_for_file_object(tmp_path):
 @fixture
 def rsa_file(tmp_path):
     my_rsa_file = RSAFile(path.join(tmp_path, 'my_key.pem'))
+    my_rsa_file.gen_pem_file()
     return my_rsa_file
+
+
+@fixture
+def encryption_file(base_file_with_content, rsa_file):
+    my_encrpytion_file = EncryptionFile(
+        base_file_with_content.get_filepath(),
+        rsa_file.get_filepath())
+    return my_encrpytion_file
 
 
 @mark.parametrize("file_path, expected_result, is_file", [
@@ -45,9 +55,9 @@ def rsa_file(tmp_path):
     ("../env_path1/.env/", True, False),
 ])
 def test_basefile_create_and_delete_filepath(env_setup_for_file_object,
-                                       file_path,
-                                       expected_result,
-                                       is_file):
+                                             file_path,
+                                             expected_result,
+                                             is_file):
     my_file = BaseFile(file_path)
     my_file.create_filepath()
     assert my_file.filepath_exists() == expected_result
@@ -58,18 +68,6 @@ def test_basefile_create_and_delete_filepath(env_setup_for_file_object,
 
 def test_basefile_get_contents_of_text_file(base_file_with_content):
     assert base_file_with_content.get_contents_of_file() == '0123456789'
-
-
-def test_basefile_get_contents_of_binary_file():
-    pass
-
-
-def test_basefile_write_data_to_text():
-    pass
-
-
-def test_basefile_write_data_to_binary():
-    pass
 
 
 def test_basefile_clear_file(base_file, base_file_with_content):
@@ -83,12 +81,16 @@ def test_basefile_str(base_file):
     assert base_file.get_filepath() == str(base_file)
 
 
+def test_encryptionfile_encrypt_and_decrypt(encryption_file):
+    encryption_file.encrypt()
+    assert encryption_file.is_binary()
+    encryption_file.decrypt()
+    assert not encryption_file.is_binary()
+
+
 def test_rsafile_gen_pem_file(rsa_file):
-    rsa_file.gen_pem_file()
-    print("**************", rsa_file.get_filepath())
     assert rsa_file.filepath_exists()
 
 
 def test_rsafile_get_key(rsa_file):
-    rsa_file.gen_pem_file()
     assert rsa_file.get_key()
